def4ea2f1309f26ea9ac9195248fd4dc9f014ed3
inwchamp1337 <ppasinb@gmail.com>
2025-03-26 15:52:56 +0700

    Delete index.ts

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-// src/types/index.ts
-import { t } from 'elysia'
-
-export const CourseSchema = t.Object({
-    id: t.Number(),
-    course_id: t.Number(),
-    name: t.String(),
-    nameTH: t.String()
-})
